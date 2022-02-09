1f2e745f1dff80eb449fbd53bd20c65fe6e18e1f
Johnle3 <89218532+Johnle3@users.noreply.github.com>
2022-02-09 11:05:21 -0500

    Add files via upload

--- Le.john.lab1.sql
+++ Le.john.lab1.sql
@@ -1,4 +1,6 @@
 SELECT * FROM northwind.customers;
+select * from order_details;
+select * from products;
 #1.	Write a query to get Product name and quantity/unit.
 Select northwind.products.product_name, northwind.products.quantity_per_unit 
 FROM Northwind.products;
@@ -10,30 +12,32 @@ Select northwind.products.id,northwind.products.product_name
 FROM northwind.products 
 WHERE northwind.products.discontinued="1" ;
 #4. Write a query to get most expense and least expensive Product list (name and unit price).  
-Select max(northwind.products.product_name),max(northwind.products.list_price),
-min(northwind.products.product_name),min(northwind.products.list_price) 
-from northwind.products;
+SELECT distinct product_name, unit_price FROM order_details JOIN products ON 
+products.id = order_details.product_id WHERE unit_price= (SELECT MAX(unit_price) FROM order_details) OR 
+unit_price= (SELECT MIN(unit_price) FROM order_details);
 #5. Write a query to get Product list (id, name, unit price) where current products cost less than $20. 
-Select northwind.products.id, northwind.products.product_name, northwind.products.list_price 
-from northwind.products 
-where northwind.products.list_price<"20";
+SELECT distinct product_id,product_name, unit_price FROM products JOIN order_details ON 
+products.id = order_details.product_id WHERE unit_cost<20;
 #6. Write a query to get Product list (id, name, unit price) where products cost between $15 and $25. 
-Select northwind.products.id, northwind.products.product_name, northwind.products.list_price 
-from northwind.products 
-where northwind.products.list_price between 15 and 20;
+SELECT distinct product_id,product_name, unit_price FROM products 
+JOIN order_details ON 
+products.id = order_details.product_id 
+WHERE unit_price between 15 and 25;
 #7. Write a query to get Product list (name, unit price) of above average price.  
-Select avg(northwind.products.list_price) from northwind.products;
-Select northwind.products.product_name, northwind.products.list_price 
-from northwind.products where northwind.products.list_price>15.84577778;
+SELECT distinct product_name, unit_price 
+FROM order_details JOIN products ON 
+products.id = order_details.product_id 
+WHERE unit_price> (SELECT AVG(unit_price) FROM order_details);
 #8. Write a query to get Product list (name, unit price) of ten most expensive products.  
-Select northwind.products.product_name, northwind.products.list_price 
-from northwind.products ORDER BY list_price DESC limit 10;
+SELECT distinct product_name, unit_price 
+FROM order_details JOIN products ON 
+products.id = order_details.product_id 
+ORDER by unit_price LIMIT 10;
 #9. Write a query to count current and discontinued products. 
-Select count(northwind.products.discontinued) from northwind.products where discontinued="0";
-Select count(northwind.products.discontinued) from northwind.products where discontinued="1";
+SELECT discontinued,COUNT(*) AS 'current' FROM mynorthwind.products GROUP BY discontinued;
 #10. Write a query to get Product list (name, units on order, units in stock) of stock is less than the quantity on order.  
-Select northwind.products.product_name, northwind.products.reorder_level, northwind.products.quantity_per_unit 
-From northwind.products 
-where northwind.products.quantity_per_unit<northwind.products.reorder_level
+SELECT products.product_name, order_details.orderQuantity, inventory_transactions.quantity FROM products JOIN 
+order_details JOIN inventory_transactions ON products.id=order_details.product_id AND order_details.product_id=inventory_transactions.product_id 
+WHERE inventory_transactions.quantity < order_details.orderQuantity;
 
 
